95ba5d64abf12f9385b89088baee8d08d11df3e0
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-20 20:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
-
+    document.querySelectorAll("strong").forEach(el => {
+        el.style.color = "rgb(0, 128, 0)";
+    });
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+	//Write your code here
+    document.querySelectorAll("strong").forEach(el => {
+        el.style.color = "rgb(0, 0, 0)";
+    });
 }
+
+document.getElementById("highlight-link").addEventListener("mouseover", highlight);
+document.getElementById("highlight-link").addEventListener("mouseout", return_normal);
+
+}
+
